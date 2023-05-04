58b148fa5439d882af7f7cee7f5f99f3ba3c16b9
August <vlad.socolov@mail.ru>
2023-05-04 09:39:00 +0300

    -Implemented keaboard char checker

--- blindtyper-react/src/App.tsx
+++ blindtyper-react/src/App.tsx
@@ -1,19 +1,28 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { InfoHeader } from "./components/InfoHeader";
 import { Quote } from "./components/Quote";
 import "./App.css";
+import { currentTime } from "./utils/time";
 
 function App() {
-  const [quote, setQuote] = useState("");
-  const [text, setText] = useState("");
+  const [quote, setQuote] = useState<Array<string>>([]);
   const [isLoading, setLoading] = useState(true);
+  const [text, setText] = useState("");
   const [typedChars, setTypedChars] = useState(0);
+  const [currentChar, setCurrentChar] = useState("");
+  const [startTime, setStartTime] = useState(0);
+  const [outgoingChars, setOutgoingChars] = useState("");
+  const [isCorrectChar, setCorrectChar] = useState(true);
+  const [wpm, setWpm] = useState(0);
+  const [cpm, setCpm] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
 
   useEffect(() => {
     async function getData(url: string) {
       const response = await fetch(url);
       const data = await response.json();
       setQuote(data);
+      initQuoteStates(data);
       setLoading(false);
     }
     getData("https://baconipsum.com/api/?type=meat-and-filler&paras=2");
@@ -27,6 +36,32 @@ function App() {
     if (event.key === "Tab") {
       event.preventDefault();
     }
+
+    initStartTime();
+
+    setTypedChars((char) => char + 1);
+
+    if (event.key === currentChar) {
+      console.log("Got a symbol match!", currentChar, event.key);
+      setCurrentChar(text.charAt(0));
+      setText(text.substring(1));
+      setOutgoingChars((prev) => prev + currentChar);
+      setCorrectChar(true);
+    } else if (event.key !== "Shift") {
+      console.log("Char mismatch");
+      setCorrectChar(false);
+    }
+  };
+
+  const initStartTime = () => {
+    if (!startTime) {
+      setStartTime(currentTime());
+    }
+  };
+
+  const initQuoteStates = (quote: string[]) => {
+    setCurrentChar(quote[0].charAt(0));
+    setText(quote[0].substring(1));
   };
 
   return (
@@ -35,7 +70,6 @@ function App() {
         <p>Loading...</p>
       ) : (
         <div className="flex">
-          <p>{text}</p>
           <input
             type="text"
             name="hiddenType"
@@ -45,7 +79,7 @@ function App() {
             onKeyDown={handleKeyDown}
             onBlur={handleBlur}
           />
-          <Quote text={quote}></Quote>
+          <Quote text={text} char={currentChar} outgoingChars={outgoingChars} isCorrectChar={isCorrectChar}></Quote>
           <InfoHeader cpm={typedChars} />
         </div>
       )}
--- blindtyper-react/src/components/Quote/Quote.tsx
+++ blindtyper-react/src/components/Quote/Quote.tsx
@@ -2,12 +2,19 @@ import "./Quote.css";
 
 interface IQuoteProps {
   text: string;
+  char: string;
+  isCorrectChar: boolean;
+  outgoingChars: string;
 }
 
-export const Quote = ({ text }: IQuoteProps) => {
+export const Quote = ({ text, char, isCorrectChar, outgoingChars }: IQuoteProps) => {
   return (
     <div className="quote">
-      <p>{text}</p>
+      <p>
+        <span className="green">{outgoingChars}</span>
+        <span className={isCorrectChar ? "green" : "red"}>{char}</span>
+        <span>{text}</span>
+      </p>
     </div>
   );
 };
