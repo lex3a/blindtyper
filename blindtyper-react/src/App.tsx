9b91ec9a41f456ea51ee0a618484b7461be2fb16
August <vlad.socolov@mail.ru>
2023-05-05 11:34:49 +0300

    -More useCallback -Increase font size a bit -Implemet reset

--- blindtyper-react/src/App.tsx
+++ blindtyper-react/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { InfoHeader } from "./components/InfoHeader";
 import { Quote } from "./components/Quote";
 import { currentTime, getDurationInMinutes } from "./utils/time";
 import "./App.css";
 
 const DEFAULT_ACCURACY = 100;
+const API_URL = "https://baconipsum.com/api/?type=meat-and-filler&paras=1";
 
 function App() {
   const [quote, setQuote] = useState("");
@@ -32,16 +33,16 @@ function App() {
     setText(newQuote.substring(1));
   }, []);
 
-  useEffect(() => {
-    async function getData(url: string) {
-      const response = await fetch(url);
-      const [data] = await response.json();
-
-      initQuoteStates(data);
-      setLoading(false);
-    }
-    getData("https://baconipsum.com/api/?type=meat-and-filler&paras=1");
+  const getQuoteData = useCallback(async () => {
+    const response = await fetch(API_URL);
+    const [data] = await response.json();
+    initQuoteStates(data);
+    setLoading(false);
   }, [initQuoteStates]);
+
+  useEffect(() => {
+    getQuoteData();
+  }, [getQuoteData]);
 
   useEffect(() => {
     intervalId.current = setInterval(() => {
@@ -59,75 +60,38 @@ function App() {
     };
   });
 
-  const calculateWpm = (wordCount: number, durationInMinutes: number) => {
+  const reset = useCallback(() => {
+    setLoading(true);
+    getQuoteData();
+    setWpm(0);
+    setCpm(0);
+    setAccuracy(DEFAULT_ACCURACY);
+    setText("");
+    setWordCount(0);
+    setCurrentChar("");
+    setStartTime(0);
+    setOutgoingChars("");
+    setCorrectChar(true);
+    setErrorChars(0);
+  }, [getQuoteData]);
+
+  const calculateWpm = useCallback((wordCount: number, durationInMinutes: number) => {
     return Math.round(wordCount / durationInMinutes);
-  };
-
-  // const calculateCpm = (charCount: number, durationInMinutes: number) => {
-  //   return Math.round(charCount / durationInMinutes);
-  // };
-
-  const calculateCpm = useMemo(
-    () => (charCount: number, durationInMinutes: number) => {
-      return Math.round(charCount / durationInMinutes);
-    },
-    []
-  );
-
-  const calculateAccuracy = (errorChars: number, originalText: string) => {
-    return Math.max((1000 - Math.round((1000 * errorChars) / originalText.length)) / 10, 0);
-  };
-
-  const handleBlur = useCallback(({ target }: React.FocusEvent<HTMLInputElement, Element>) => {
-    target.focus();
   }, []);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const { key } = event;
+  const calculateCpm = useCallback((charCount: number, durationInMinutes: number) => {
+    return Math.round(charCount / durationInMinutes);
+  }, []);
 
-    if (key === "Tab") {
-      event.preventDefault();
-    }
+  const calculateAccuracy = useCallback((errorChars: number, originalText: string) => {
+    return Math.max((1000 - Math.round((1000 * errorChars) / originalText.length)) / 10, 0);
+  }, []);
 
-    let updatedOutgoingChars = outgoingChars;
-
-    initStartTime();
-
-    if (key.length === 1) {
-      if (key === currentChar) {
-        setCurrentChar(text.charAt(0));
-        setText(text.substring(1));
-        updatedOutgoingChars += currentChar;
-        setOutgoingChars(updatedOutgoingChars);
-        setCorrectChar(true);
-        const durationInMinutes = getDurationInMinutes(startTime);
-        const newCpm = calculateCpm(updatedOutgoingChars.length, durationInMinutes);
-        setCpm(newCpm);
-
-        if (text.charAt(0) === " ") {
-          const durationInMinutes = getDurationInMinutes(startTime);
-          const newWordCount = wordCount + 1;
-          const newWpm = calculateWpm(newWordCount, durationInMinutes);
-          setWpm(newWpm);
-          setWordCount(newWordCount);
-        }
-      } else {
-        if (!isCorrectChar) return; // exit if last try was already incorrect
-
-        const newError = errorChars + 1;
-        const newAccuracy = calculateAccuracy(newError, quote);
-        setCorrectChar(false);
-        setAccuracy(newAccuracy);
-        setErrorChars(newError);
-      }
-    }
-  };
-
-  const initStartTime = () => {
+  const initStartTime = useCallback(() => {
     if (!startTime) {
       setStartTime(currentTime());
     }
-  };
+  }, [startTime]);
 
   const isFinished = () => {
     const finished = outgoingChars.length === quote.length;
@@ -138,6 +102,67 @@ function App() {
 
     return finished;
   };
+
+  const handleBlur = useCallback(({ target }: React.FocusEvent<HTMLInputElement, Element>) => {
+    target.focus();
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const { key } = event;
+
+      if (key === "Tab") {
+        event.preventDefault();
+      }
+
+      let updatedOutgoingChars = outgoingChars;
+
+      initStartTime();
+
+      if (key.length === 1) {
+        if (key === currentChar) {
+          setCurrentChar(text.charAt(0));
+          setText(text.substring(1));
+          updatedOutgoingChars += currentChar;
+          setOutgoingChars(updatedOutgoingChars);
+          setCorrectChar(true);
+          const durationInMinutes = getDurationInMinutes(startTime);
+          const newCpm = calculateCpm(updatedOutgoingChars.length, durationInMinutes);
+          setCpm(newCpm);
+
+          if (text.charAt(0) === " ") {
+            const durationInMinutes = getDurationInMinutes(startTime);
+            const newWordCount = wordCount + 1;
+            const newWpm = calculateWpm(newWordCount, durationInMinutes);
+            setWpm(newWpm);
+            setWordCount(newWordCount);
+          }
+        } else {
+          if (!isCorrectChar) return; // exit if last try was already incorrect
+
+          const newError = errorChars + 1;
+          const newAccuracy = calculateAccuracy(newError, quote);
+          setCorrectChar(false);
+          setAccuracy(newAccuracy);
+          setErrorChars(newError);
+        }
+      }
+    },
+    [
+      calculateAccuracy,
+      calculateCpm,
+      calculateWpm,
+      currentChar,
+      errorChars,
+      initStartTime,
+      isCorrectChar,
+      outgoingChars,
+      quote,
+      startTime,
+      text,
+      wordCount,
+    ]
+  );
 
   return (
     <>
@@ -158,11 +183,13 @@ function App() {
             <div>
               <p>You nailed it! Literally :D</p>
               <InfoHeader cpm={cpm} wpm={wpm} accuracy={accuracy} />
+              <button onClick={reset}>Reset</button>
             </div>
           ) : (
             <>
               <Quote text={text} char={currentChar} outgoingChars={outgoingChars} isCorrectChar={isCorrectChar}></Quote>
               <InfoHeader cpm={cpm} wpm={wpm} accuracy={accuracy} />
+              <button onClick={reset}>Reset</button>
             </>
           )}
         </div>
