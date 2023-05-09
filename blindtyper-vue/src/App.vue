a72cfb48a771d5cad78b00eaa192b349e612d36b
August <vlad.socolov@mail.ru>
2023-05-09 23:14:24 +0300

    -[Vue] Move almost everything to store

--- blindtyper-vue/src/App.vue
+++ blindtyper-vue/src/App.vue
@@ -2,56 +2,17 @@
 import { onMounted, onUnmounted, ref, watchEffect } from 'vue';
 import InfoHeader from './components/InfoHeader.vue'
 import Quote from './components/Quote.vue'
-import { currentTime, getDurationInMinutes } from './utils/time';
+import { getDurationInMinutes } from './utils/time';
 import { checkWorldRecord } from './utils/record';
-import { calculateAccuracy, calculateCpm, calculateWpm } from './utils/stats';
+import { useKeyboardTyperStore } from './stores/KeyboardTyperStore';
 
-
-const DEFAULT_ACCURACY = 100;
-const API_URL = 'https://baconipsum.com/api/?type=meat-and-filler&paras=1';
-
-const quote = ref('');
-const wpm = ref(0);
-const cpm = ref(0);
-const accuracy = ref(DEFAULT_ACCURACY);
-const isLoading = ref(true);
-const text = ref('');
-const wordCount = ref(0);
-const currentChar = ref('');
-const startTime = ref(0);
-const outgoingChars = ref('');
-const isCorrectChar = ref(true);
-const errorChars = ref(0);
-const fetchError = ref('');
 const intervalId = ref<number>();
 const input = ref<HTMLInputElement | null>(null);
 
-const initQuoteStates = (quoteText: string) => {
-  const newQuote = quoteText.replace(/\s{2}/g, ' ');
-  quote.value = newQuote;
-  currentChar.value = newQuote.charAt(0);
-  text.value = newQuote.substring(1);
-};
-
-const getQuoteData = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const [data] = await response.json();
-    initQuoteStates(data);
-    isLoading.value = false;
-  } catch (error) {
-    fetchError.value = `An error fetching quote. ${error}`;
-  }
-};
-
-const initStartTime = () => {
-  if (!startTime.value) {
-    startTime.value = currentTime();
-  }
-};
+const store = useKeyboardTyperStore();
 
 const isFinished = () => {
-  const finished = outgoingChars.value.length === quote.value.length;
+  const finished = store.outgoingChars.length === store.quote.length;
 
   if (finished) {
     clearInterval(intervalId.value);
@@ -60,35 +21,9 @@ const isFinished = () => {
   return finished;
 };
 
-const reset = () => {
-  isLoading.value = true;
-  getQuoteData();
-  wpm.value = 0;
-  cpm.value = 0;
-  accuracy.value = DEFAULT_ACCURACY;
-  text.value = '';
-  wordCount.value = 0;
-  currentChar.value = '';
-  startTime.value = 0;
-  outgoingChars.value = '';
-  isCorrectChar.value = true;
-  errorChars.value = 0;
-}
-
 const handleBlur = (event: Event) => {
   (event.target as HTMLInputElement).focus();
 };
-
-const updateCpmAndWpm = () => {
-  if (startTime.value) {
-    const durationInMinutes = getDurationInMinutes(startTime.value);
-    const newCpm = calculateCpm(outgoingChars.value.length, durationInMinutes);
-    const newWpm = calculateWpm(wordCount.value, durationInMinutes);
-    cpm.value = newCpm;
-    wpm.value = newWpm;
-
-  }
-}
 
 function disableTabKey(this: Window, event: KeyboardEvent) {
   const { key } = event;
@@ -97,51 +32,22 @@ function disableTabKey(this: Window, event: KeyboardEvent) {
   }
 }
 
-const handleErrorChar = () => {
-  if (!isCorrectChar.value) return; // exit if last try was already incorrect
-
-  const newError = errorChars.value + 1;
-  const newAccuracy = calculateAccuracy(newError, quote.value);
-
-  isCorrectChar.value = false;
-  accuracy.value = newAccuracy;
-  errorChars.value = newError;
-}
-
-const handleNewChar = (durationInMinutes: number) => {
-  let updatedOutgoingChars = outgoingChars.value;
-  updatedOutgoingChars += currentChar.value;
-  outgoingChars.value = updatedOutgoingChars;
-  currentChar.value = text.value.charAt(0)
-  text.value = text.value.substring(1)
-  isCorrectChar.value = true;
-
-  const newCpm = calculateCpm(updatedOutgoingChars.length, durationInMinutes);
-  cpm.value = (newCpm);
-}
-
-const handleNewWord = (durationInMinutes: number) => {
-  const newWordCount = wordCount.value + 1;
-  const newWpm = calculateWpm(newWordCount, durationInMinutes);
-  wordCount.value = newWordCount;
-  wpm.value = newWpm;
-}
-
 const handleKeyDown = (event: KeyboardEvent) => {
   const { key } = event;
-  initStartTime();
+
+  store.initStartTime();
 
   // Check only real characters, don't check e.g. "Tab", "Shift", etc.
   if (key.length === 1) {
-    const durationInMinutes = getDurationInMinutes(startTime.value);
-    if (key === currentChar.value) {
-      handleNewChar(durationInMinutes);
+    const durationInMinutes = getDurationInMinutes(store.startTime);
+    if (key === store.currentChar) {
+      store.handleNewChar(durationInMinutes);
 
-      if (text.value.charAt(0) === " ") {
-        handleNewWord(durationInMinutes);
+      if (store.text.charAt(0) === " ") {
+        store.handleNewWord(durationInMinutes);
       }
     } else {
-      handleErrorChar();
+      store.handleErrorChar();
     }
   }
 }
@@ -155,8 +61,8 @@ watchEffect(() => {
 
 onMounted(() => {
   window.addEventListener("keydown", disableTabKey);
-  getQuoteData();
-  intervalId.value = setInterval(updateCpmAndWpm, 200);
+  store.getQuoteData();
+  intervalId.value = setInterval(store.updateCpmAndWpm, 200);
 });
 
 onUnmounted(() => {
@@ -167,22 +73,23 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <template v-if="fetchError">{{ fetchError }}</template>
-  <template v-else-if="isLoading">Loading...</template>
+  <template v-if="store.fetchError">{{ store.fetchError }}</template>
+  <template v-else-if="store.isLoading">Loading...</template>
   <template v-else-if="isFinished()">
-    <p>{{ checkWorldRecord(cpm) }}</p>
-    <InfoHeader :cpm="cpm" :wpm="wpm" :accuracy="accuracy" />
-    <button @click="reset">Reset</button>
+    <p>{{ checkWorldRecord(store.cpm) }}</p>
+    <InfoHeader :cpm="store.cpm" :wpm="store.wpm" :accuracy="store.accuracy" />
+    <button @click="store.reset">Reset</button>
   </template>
   <template v-else>
     <div class="flex">
       <input name="hiddenType" ref="input" id="hiddenType" autoFocus="true" autoComplete="off" @keydown="handleKeyDown"
         @blur="handleBlur" />
       <div class="quote-block">
-        <Quote :text="text" :char="currentChar" :outgoingChars="outgoingChars" :isCorrectChar="isCorrectChar" />
-        <button class="quote-block_button" @click="reset">Reset</button>
+        <Quote :text="store.text" :char="store.currentChar" :outgoingChars="store.outgoingChars"
+          :isCorrectChar="store.isCorrectChar" />
+        <button class="quote-block_button" @click="store.reset">Reset</button>
       </div>
-      <InfoHeader :wpm="wpm" :cpm="cpm" :accuracy="accuracy" />
+      <InfoHeader :wpm="store.wpm" :cpm="store.cpm" :accuracy="store.accuracy" />
     </div>
   </template>
 </template>
--- blindtyper-vue/src/stores/KeyboardTyperStore.ts
+++ blindtyper-vue/src/stores/KeyboardTyperStore.ts
@@ -1,6 +1,126 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { currentTime, getDurationInMinutes } from "../utils/time";
+import { calculateAccuracy, calculateCpm, calculateWpm } from "../utils/stats";
+
+const API_URL = "https://baconipsum.com/api/?type=meat-and-filler&paras=1";
+const DEFAULT_ACCURACY = 100;
 
 export const useKeyboardTyperStore = defineStore("typer", () => {
-  return {};
+  const quote = ref("");
+  const isLoading = ref(true);
+  const fetchError = ref("");
+  const currentChar = ref("");
+  const text = ref("");
+
+  const startTime = ref(0);
+
+  const wpm = ref(0);
+  const cpm = ref(0);
+  const accuracy = ref(DEFAULT_ACCURACY);
+  const wordCount = ref(0);
+  const errorChars = ref(0);
+  const outgoingChars = ref("");
+  const isCorrectChar = ref(true);
+
+  const initQuoteStates = (quoteText: string) => {
+    // Fix two spaces after the sentence which we got from API
+    const newQuote = quoteText.replace(/\s{2}/g, " ");
+    quote.value = newQuote;
+    currentChar.value = newQuote.charAt(0);
+    text.value = newQuote.substring(1);
+  };
+
+  const getQuoteData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const [data] = await response.json();
+      initQuoteStates(data);
+      isLoading.value = false;
+    } catch (error) {
+      fetchError.value = `An error fetching quote. ${error}`;
+    }
+  };
+
+  const initStartTime = () => {
+    if (!startTime.value) {
+      startTime.value = currentTime();
+    }
+  };
+
+  const reset = () => {
+    isLoading.value = true;
+    getQuoteData();
+    wpm.value = 0;
+    cpm.value = 0;
+    accuracy.value = DEFAULT_ACCURACY;
+    text.value = "";
+    wordCount.value = 0;
+    currentChar.value = "";
+    startTime.value = 0;
+    outgoingChars.value = "";
+    isCorrectChar.value = true;
+    errorChars.value = 0;
+  };
+
+  const updateCpmAndWpm = () => {
+    if (startTime.value) {
+      const durationInMinutes = getDurationInMinutes(startTime.value);
+      const newCpm = calculateCpm(outgoingChars.value.length, durationInMinutes);
+      const newWpm = calculateWpm(wordCount.value, durationInMinutes);
+      cpm.value = newCpm;
+      wpm.value = newWpm;
+    }
+  };
+
+  const handleErrorChar = () => {
+    if (!isCorrectChar.value) return; // exit if last try was already incorrect
+
+    const newError = errorChars.value + 1;
+    const newAccuracy = calculateAccuracy(newError, quote.value);
+
+    isCorrectChar.value = false;
+    accuracy.value = newAccuracy;
+    errorChars.value = newError;
+  };
+
+  const handleNewChar = (durationInMinutes: number) => {
+    let updatedOutgoingChars = outgoingChars.value;
+    updatedOutgoingChars += currentChar.value;
+    outgoingChars.value = updatedOutgoingChars;
+    currentChar.value = text.value.charAt(0);
+    text.value = text.value.substring(1);
+    isCorrectChar.value = true;
+
+    const newCpm = calculateCpm(updatedOutgoingChars.length, durationInMinutes);
+    cpm.value = newCpm;
+  };
+
+  const handleNewWord = (durationInMinutes: number) => {
+    const newWordCount = wordCount.value + 1;
+    const newWpm = calculateWpm(newWordCount, durationInMinutes);
+    wordCount.value = newWordCount;
+    wpm.value = newWpm;
+  };
+
+  return {
+    getQuoteData,
+    reset,
+    initStartTime,
+    updateCpmAndWpm,
+    handleErrorChar,
+    handleNewWord,
+    handleNewChar,
+    wpm,
+    cpm,
+    accuracy,
+    outgoingChars,
+    quote,
+    fetchError,
+    isLoading,
+    currentChar,
+    text,
+    startTime,
+    isCorrectChar,
+  };
 });
